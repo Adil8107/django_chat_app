b32bcf7a8f352e46fbdcd28a521ca4466514343b
Adil8107 <adilr8765@gmail.com>
2026-04-12 22:47:37 +0530

    Refactor routing for ASGI with ProtocolTypeRouter

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,14 @@
-from django.urls import re_path
-from .consumers import ChatConsumer
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
-]
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+django_asgi_app = get_asgi_application()
+
+import chat.routing
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": URLRouter(chat.routing.websocket_urlpatterns),
+})
