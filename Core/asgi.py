df2fe0f39e4b9e89e2839b13f5f6d54a74d41b70
Adil8107 <adilr8765@gmail.com>
2026-04-12 22:46:51 +0530

    Refactor ASGI configuration by removing django.setup()

--- Core/asgi.py
+++ Core/asgi.py
@@ -1,11 +1,9 @@
 import os
-import django
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
